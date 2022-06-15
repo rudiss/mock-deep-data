e81da5294123d863bf7e48342aadb5c57d2c8fb1
Rafael Machado <rafael.machado@sanar.com>
2022-06-15 19:24:39 -0300

    fix: vercel deploy

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const security = require('./api/security');
 
 const PORT = process.env.PORT || 5050;
 
+
+
+
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile('data.json', {
+    root: path.join(__dirname, 'public')
+  });
+})
+
 app.use('/api/security', security);
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
